808543d8825c00151d985d99b714038e431d8e97
Sebastian Sobociński <sebastian@soft-bind.com>
2020-09-25 11:46:16 +0200

    fix(build): remove named export

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,6 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   external: ['child_process'],
-  output: [{ file: pkg.main, format: 'cjs', exports: 'named' }],
+  output: [{ file: pkg.main, format: 'cjs' }],
   plugins: [babel()],
 }
